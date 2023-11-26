439669cdb7249fe3a0a32244209f13730ac62ee2
tundrajänes <34316477+mahlane-arbuus@users.noreply.github.com>
2023-11-27 00:19:41 +0200

    upgrades menu
    added upgrade menu beta version

---
+++ WrathOfTheOctopus/Assets/Scripts/Book/UpgradeIcon.cs
@@ -0,0 +1,46 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
+
+public class UpgradeIcon : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public string Name;
+    public string Description;
+    public string Requirements;
+    public TextMeshProUGUI NameText;
+    public TextMeshProUGUI DescriptionText;
+    public TextMeshProUGUI RequirementsText;
+
+    private Image image;
+    private Color highlightColor = new Color(0, 0, 0);
+    private Color normalColor = new Color(0, 0, 0, 120);
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        NameText.text = Name + ":";
+        DescriptionText.text = Description;
+        RequirementsText.text = "Requirements:" + Environment.NewLine + Environment.NewLine + Requirements;
+        image.color = highlightColor;
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        NameText.text = "";
+        DescriptionText.text = "";
+        RequirementsText.text = "";
+        image.color = normalColor;
+    }
+}
