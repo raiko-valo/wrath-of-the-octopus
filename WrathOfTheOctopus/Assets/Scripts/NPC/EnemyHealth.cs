c704fcd91d1b2951b81a5c06664c7f3e5f6fc5f7
Iris Kreinin <iris.kreinin@ut.ee>
2023-11-26 22:26:28 +0200

    Added animations conditions in controllers

--- WrathOfTheOctopus/Assets/Scripts/NPC/EnemyHealth.cs
+++ WrathOfTheOctopus/Assets/Scripts/NPC/EnemyHealth.cs
@@ -6,13 +6,22 @@ public class EnemyHealth : MonoBehaviour
 {
     [SerializeField]
     private int health = 0;
+    private Animator animator;
 
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     public void OnRemoveHealth(int amount)
     {
+        
         health -= amount;
-
+        animator.SetBool("Hurt", true);
+        animator.SetBool("Hurt", false);
         if (health <= 0)
         {
+            animator.SetInteger("Health", 0);
             Destroy(gameObject);
         }
     }
--- WrathOfTheOctopus/Assets/Scripts/NPC/NpcAttack.cs
+++ WrathOfTheOctopus/Assets/Scripts/NPC/NpcAttack.cs
@@ -7,12 +7,15 @@ public class NpcAttack : MonoBehaviour
     public float NextAttack;
     public float AttackCooldown;
     public int Damage;
+    private Animator animator;
+
 
     private Health healt;
 
     private void Start()
     {
         NextAttack = Time.time;
+        animator = GetComponent<Animator>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -22,8 +25,10 @@ public class NpcAttack : MonoBehaviour
         if (health != null && Time.time >= NextAttack)
         {
             Events.RemoveHealth(Damage);
+            animator.SetBool("Attack", true);
             NextAttack += AttackCooldown;
         }
+        animator.SetBool("Attack", false);
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -32,7 +37,9 @@ public class NpcAttack : MonoBehaviour
         if (health != null && Time.time >= NextAttack)
         {
             Events.RemoveHealth(Damage);
+            animator.SetBool("Attack", true);
             NextAttack += AttackCooldown;
         }
+        animator.SetBool("Attack", false);
     }
 }
